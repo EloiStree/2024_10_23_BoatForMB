60e1c9ac7a68006b2c1f9692b4c9525b7e35c48c
Eloi Strée <eloistree@gmail.com>
2024-10-23 12:15:06 +0200

    Save some script and folder

---
+++ Script/BasicBoatEditorMono.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Eloi
+{
+    [CustomEditor(typeof(BasicBoatMono))]
+    public class BasicBoatEditorMono: Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            BasicBoatMono t = (BasicBoatMono)target;
+            if (GUILayout.Button("Teleport Forward"))
+            {
+                t.TeleportForward();
+            }
+        }
+    }
+}
